59ddeecf4bd6023dcdb73c180c1aa0e7f5e9f657
natsuki.yamada.ubuntu6 <natsuking.smile09@gmail.com>
2019-09-12 07:03:11 +0000

    unknown_userモデルの作成とunknown_usersコントローラの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_08_044415) do
+ActiveRecord::Schema.define(version: 2019_09_12_065131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,11 @@ ActiveRecord::Schema.define(version: 2019_09_08_044415) do
     t.string "content"
     t.index ["event_code"], name: "index_events_on_event_code", unique: true
     t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "unknown_users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
