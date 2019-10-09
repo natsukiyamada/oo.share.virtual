2b22aaf5be7f46b55cffb5f228dd19a5f4033d20
natsuki.yamada.ubuntu6 <natsuking.smile09@gmail.com>
2019-10-09 11:21:52 +0000

    いいね機能 ajax化　実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,14 +3,22 @@ class LikesController < ApplicationController
   #before_action :get_path_to_event_show, only: [:create, :destroy]
   
   def create
-    
     like = current_user.likes.build(comment_id: params[:comment_id])
-    comment = Comment.find_by(id: params[:comment_id])
-    event = comment.event
-    user = event.user
     
-    like.save
-    redirect_to user_event_path(user, event)
+    if params[:comment_id].present? 
+      @comment = Comment.find_by(id: params[:comment_id])
+      event = @comment.event
+      user = event.user
+    end
+    
+    respond_to do |format|  
+      if like.save
+        format.js { render :create}
+      else
+        flash.alert = "いいねに失敗しました"
+        format.html {redirect_to user_event_path(user, event)}
+      end
+    end
   end
 
   def destroy
@@ -20,11 +28,19 @@ class LikesController < ApplicationController
   	  like = Like.find_by(comment_id: params[:comment_id], user_id: current_user.id)
   	end 
     
-    comment = Comment.find_by(id: params[:comment_id])
-    event = comment.event
+    @comment = Comment.find_by(id: params[:comment_id])
+    event = @comment.event
     user = event.user
-    like.destroy
-    redirect_to  user_event_path(user, event)
+    
+    respond_to do |format|
+    
+      if like.destroy
+        format.js {render :destroy}
+      else
+        flash.alert = "いいねに失敗しました"
+        format.html {redirect_to user_event_path(user, event)}
+      end
+    end
   end
   
   private
