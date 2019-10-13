7b2548395da8a0e41c2cc5fb4188a2368e825b89
natsuki.yamada.ubuntu6 <natsuking.smile09@gmail.com>
2019-10-13 15:30:12 +0000

    views/users/show jqueryのアニメーション改善

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,7 @@
 $(document).on('turbolinks:load', function() { 
     $(function(){
       $('.user_edit').hover(function(){
-          $( ".dropdown_menu_user_edit").slideDown('fast');
+          $(".dropdown_menu_user_edit:not(:animated)", this).slideDown('fast');
       }, function(){
           $(".dropdown_menu_user_edit").slideUp('fast');
       });
